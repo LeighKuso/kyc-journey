85c825bbca7529aa128e1230d613e36112db9159
Ashleigh <leighkuso@gmail.com>
2025-09-16 18:23:32 +0200

    update server config for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 8080
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
